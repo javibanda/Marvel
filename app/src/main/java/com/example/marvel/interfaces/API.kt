4d27eb5a73ea6236507bea4a7324f549e015d9be
narutojavibanda <narutojavibanda@gmail.com>
2021-07-22 18:50:32 +0200

    i

--- app/src/main/java/com/example/marvel/interfaces/API.kt
+++ app/src/main/java/com/example/marvel/interfaces/API.kt
@@ -6,9 +6,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object API  {
     private const val BASE_URL = "http://gateway.marvel.com"
-    private const val TS = "1"
-    private const val API_KEY = "6253edfe88d0a0142ee64ad46a162160"
-    private const val HASH = "97bc35c8096b31be0da8bd253480c66f"
 
 
     val retrofit: ApiService by lazy{
