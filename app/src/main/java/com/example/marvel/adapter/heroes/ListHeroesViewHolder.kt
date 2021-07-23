44c1834bba1a2cb7a78ec1078c06fba9bbae43d7
javi <narutojavibanda@gmail.com>
2021-07-23 16:36:59 +0200

    Create ListHeroesViewHolder.kt

---
+++ app/src/main/java/com/example/marvel/adapter/heroes/ListHeroesViewHolder.kt
@@ -0,0 +1,13 @@
+package com.example.marvel.adapter.heroes
+
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.marvel.adapter.base.BaseViewHolder
+import com.example.marvel.model.Result
+
+class ListHeroesViewHolder(itemView: View) : BaseViewHolder(itemView) {
+
+    fun bindHeroes(heroes: Result, fragment: Fragment){
+
+    }
+}
