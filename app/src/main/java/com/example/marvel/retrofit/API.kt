1e4f8d7f950a214dc5586de63263fb2e78e0a567
narutojavibanda <narutojavibanda@gmail.com>
2021-07-23 19:44:56 +0200

    Reg¡factor retrofit

--- app/src/main/java/com/example/marvel/retrofit/API.kt
+++ app/src/main/java/com/example/marvel/retrofit/API.kt
@@ -1,21 +1,14 @@
-package com.example.marvel.interfaces
+package com.example.marvel.retrofit
 
-import android.util.Log
-import com.example.marvel.model.Data
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
 object API  {
-    const val BASE_URL = "http://gateway.marvel.com"
-    const val TS = "1"
-    const val API_KEY = "6253edfe88d0a0142ee64ad46a162160"
-    const val HASH = "97bc35c8096b31be0da8bd253480c66f"
+    private const val BASE_URL = "http://gateway.marvel.com"
+
 
     private fun getHttpLoggingInterceptor() =
         HttpLoggingInterceptor()
---
+++ app/src/main/java/com/example/marvel/retrofit/ApiService.kt
@@ -0,0 +1,24 @@
+package com.example.marvel.retrofit
+
+
+import com.example.marvel.model.characters.Characters
+import com.example.marvel.model.characters.Heroes
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("/v1/public/characters?")
+    fun getCharacters(@Query("ts") ts: String = TS,
+                      @Query("apikey") apiKey: String = API_KEY,
+                      @Query("hash") hash: String = HASH
+    ): Call<Characters>
+
+    companion object{
+        const val TS = "1"
+        const val API_KEY = "6253edfe88d0a0142ee64ad46a162160"
+        const val HASH = "97bc35c8096b31be0da8bd253480c66f"
+    }
+
+}
