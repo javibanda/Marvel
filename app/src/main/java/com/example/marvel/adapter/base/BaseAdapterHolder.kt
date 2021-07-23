3d2a3b9444b52f0164d6b786a73e7d4880a95957
javi <narutojavibanda@gmail.com>
2021-07-23 16:17:12 +0200

    Create interface BaseAdapterHolder.kt

---
+++ app/src/main/java/com/example/marvel/adapter/base/BaseAdapterHolder.kt
@@ -0,0 +1,15 @@
+package com.example.marvel.adapter.base
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+
+interface BaseAdapterHolder {
+    @LayoutRes
+    fun getViewHolderLayoutId(viewType: Int): Int
+
+    fun createView(parent: ViewGroup, viewType: Int): View =
+        LayoutInflater.from(parent.context)
+            .inflate(getViewHolderLayoutId(viewType), parent, false)
+}
