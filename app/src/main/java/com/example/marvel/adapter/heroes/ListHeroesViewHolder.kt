ddedeabee945284f4d5e2d503e96e71d81f89c33
javi <narutojavibanda@gmail.com>
2021-07-24 01:18:40 +0200

    Init views

--- app/src/main/java/com/example/marvel/adapter/heroes/ListHeroesViewHolder.kt
+++ app/src/main/java/com/example/marvel/adapter/heroes/ListHeroesViewHolder.kt
@@ -1,13 +1,23 @@
 package com.example.marvel.adapter.heroes
 
+import android.media.Image
+import android.view.TextureView
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.marvel.R
 import com.example.marvel.adapter.base.BaseViewHolder
 import com.example.marvel.model.characters.Heroes
+import com.example.marvel.util.loadUrl
 
 class ListHeroesViewHolder(itemView: View) : BaseViewHolder(itemView) {
 
-    fun bindHeroes(heroes: Heroes, fragment: Fragment){
+    private val txtName = itemView.findViewById<TextView>(R.id.txtNameHero)
+    private val imgHero = itemView.findViewById<ImageView>(R.id.imgHero)
 
+    fun bindHeroes(heroes: Heroes, fragment: Fragment){
+        txtName.text = heroes.name
+        imgHero.loadUrl(heroes.thumbnail.getUrl())
     }
 }
