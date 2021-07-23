862fde11e273f0c93b8c84cff0aeb0bc6a7421c9
javi <narutojavibanda@gmail.com>
2021-07-24 01:20:34 +0200

    Delete all test code

--- app/src/main/java/com/example/marvel/MainActivity.kt
+++ app/src/main/java/com/example/marvel/MainActivity.kt
@@ -2,49 +2,11 @@ package com.example.marvel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.example.marvel.retrofit.API
-import com.example.marvel.model.characters.Characters
-import com.example.marvel.model.characters.Heroes
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getCurrentData()
-
     }
-
-    private fun getCurrentData(){
-        val call = API.retrofit.getCharacters()
-        call.enqueue(object : Callback<Characters> {
-            override fun onResponse(call: Call<Characters>, response: Response<Characters>) {
-                if (response.code() == 200){
-
-                    for (heroes in response.body()?.data?.heroes!!){
-                        Log.d(":::Retrofit", heroes.toString())
-                    }
-
-
-
-                }else{
-                    Log.d(":::Retrofit", "ERROR")
-                }
-            }
-
-            override fun onFailure(call: Call<Characters>, t: Throwable) {
-
-                t.message?.let { Log.d(":::Retrofit", it) }
-            }
-
-        })
-    }
-
-
-
 }
