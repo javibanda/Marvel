e2b112d384b281cbaba59e520ee56e05b53f342e
javi <narutojavibanda@gmail.com>
2021-07-24 01:12:28 +0200

    Create attribute count

--- app/src/main/java/com/example/marvel/model/characters/Data.kt
+++ app/src/main/java/com/example/marvel/model/characters/Data.kt
@@ -3,8 +3,9 @@ package com.example.marvel.model.characters
 import com.google.gson.annotations.SerializedName
 
 data class Data(
-    @SerializedName("results")
-    val heroes: List<Heroes>
+    @SerializedName("results") val heroes: List<Heroes>,
+    @SerializedName("count")
+    val count: Int
 ) {
     override fun toString(): String {
         return "Data(results=$heroes)"
