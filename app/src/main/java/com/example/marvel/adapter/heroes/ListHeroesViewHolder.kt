029c512bc20d4a100b02772a7aa14103df77142a
narutojavibanda <narutojavibanda@gmail.com>
2021-07-25 18:03:55 +0200

    Navigate to Hero Fragment

--- app/src/main/java/com/example/marvel/adapter/heroes/ListHeroesViewHolder.kt
+++ app/src/main/java/com/example/marvel/adapter/heroes/ListHeroesViewHolder.kt
@@ -4,8 +4,10 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 import com.example.marvel.R
 import com.example.marvel.adapter.base.BaseViewHolder
+import com.example.marvel.fragments.ListHeroesFragmentDirections
 import com.example.marvel.model.characters.Hero
 import com.example.marvel.util.loadUrl
 
@@ -17,5 +19,27 @@ class ListHeroesViewHolder(itemView: View) : BaseViewHolder(itemView) {
     fun bindHeroes(hero: Hero, fragment: Fragment){
         txtName.text = hero.name
         imgHero.loadUrl(hero.thumbnail.getUrl())
+        click(fragment, hero)
+    }
+
+    private fun initNavigate(hero: Hero ) =
+        ListHeroesFragmentDirections.actionListHeroesFragmentToHeroFragment(
+            hero.id,
+            hero.name,
+            hero.description,
+            hero.thumbnail.getUrl()
+        )
+
+
+    private fun navigate(fragment: Fragment, hero: Hero){
+        NavHostFragment.findNavController(fragment).navigate(
+            initNavigate(hero))
+    }
+
+    private fun click(fragment: Fragment, hero: Hero){
+        itemView.setOnClickListener {
+            navigate(fragment, hero)
+        }
+
     }
 }
