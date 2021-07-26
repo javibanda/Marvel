d2a4c43160183e187f7d500bcbe18be6d4aa9d40
javi <narutojavibanda@gmail.com>
2021-07-26 19:20:35 +0200

    Refactor Class

--- app/src/main/java/com/example/marvel/model/characters/Thumbnail.kt
+++ app/src/main/java/com/example/marvel/model/characters/Thumbnail.kt
@@ -5,4 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class Thumbnail(
     private val extension: String,
     private val path: String,
-)
+) {
+    fun getUrl(): String = "$path.$extension"
+
+}
